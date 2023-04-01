c2a56e78c33a585ee71798580e28bd4ee86d7b77
Jhony10-bit <johnny.aj.antonio@gmail.com>
2023-04-02 02:39:11 +0300

    BigUpdate

--- chat-server.js
+++ chat-server.js
@@ -7,6 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const port = process.env.PORT || 4000;
+
 async function connectToDatabase() {
   const uri = 'mongodb+srv://jhony-33:Serafim12@cluster0.j3va4xj.mongodb.net/ChatsDatabase?retryWrites=true&w=majority'; // Replace with your own MongoDB Atlas URI
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -14,8 +16,11 @@ async function connectToDatabase() {
   return client.db().collection("ChatsLog");
 }
 
+let userCount = 0;
+
 wss.on('connection', async (ws) => {
   console.log('a user connected');
+  userCount++;
 
   const messagesCollection = await connectToDatabase();
 
@@ -31,6 +36,7 @@ wss.on('connection', async (ws) => {
           client.send(JSON.stringify({ type: 'notification', message: joinMessage }));
         }
       });
+      userCount++;
     } else if (messageObj.type === 'message') {
       const message = `${messageObj.name}: ${messageObj.message}`;
       wss.clients.forEach((client) => {
@@ -40,7 +46,24 @@ wss.on('connection', async (ws) => {
       });
       await saveMessage(messagesCollection, messageObj);
     }
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: 'userCount', count: userCount }));
+      }
+    });
   });
+
+  ws.on('close', () => {
+    console.log('a user disconnected');
+    userCount--;
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: 'userCount', count: userCount }));
+      }
+    });
+  });
+
+  ws.send(JSON.stringify({ type: 'userCount', count: userCount }));
 });
 
 async function saveMessage(collection, messageObj) {
@@ -59,84 +82,6 @@ async function saveMessage(collection, messageObj) {
 }
 
 
-server.listen(4000, () => {
-  console.log('Server listening on port 4000');
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const express = require('express');
-//const http = require('http');
-//const WebSocket = require('ws');
-//const { MongoClient } = require('mongodb');
-//
-//const app = express();
-//const server = http.createServer(app);
-//const wss = new WebSocket.Server({ server });
-//
-//async function connectToDatabase() {
-//  const uri = 'mongodb+srv://jhony-33:Serafim12@cluster0.j3va4xj.mongodb.net/ChatsDatabase?retryWrites=true&w=majority';
-//  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-//  await client.connect();
-//  return client.db().collection("ChatsLog");
-//}
-//
-//wss.on('connection', async (ws) => {
-//  console.log('a user connected');
-//
-//  const messagesCollection = await connectToDatabase();
-//
-//  ws.on('message', async (data) => {
-//    console.log(`received message: ${data}`);
-//
-//    const messageObj = JSON.parse(data);
-//
-//    if (messageObj.type === 'join') {
-//      const joinMessage = `${messageObj.name} has joined the chat`;
-//      wss.clients.forEach((client) => {
-//        if (client.readyState === WebSocket.OPEN) {
-//          client.send(JSON.stringify({ type: 'notification', message: joinMessage }));
-//        }
-//      });
-//    } else if (messageObj.type === 'message') {
-//      const message = `${messageObj.name}: ${messageObj.message}`;
-//      wss.clients.forEach((client) => {
-//        if (client.readyState === WebSocket.OPEN) {
-//          client.send(JSON.stringify({ type: 'message', message }));
-//        }
-//      });
-//      await saveMessage(messagesCollection, messageObj);
-//    }
-//  });
-//});
-//
-//async function saveMessage(collection, messageObj) {
-//  const chatMessage = {
-//    name: messageObj.name,
-//    message: messageObj.message,
-//    timestamp: new Date(),
-//  };
-//  collection.insertOne(chatMessage, (error, result) => {
-//    if (error) {
-//      console.log('Error saving message:', error);
-//    } else {
-//      console.log('Message saved successfully');
-//    }
-//  });
-//}
-//
-//server.listen(4000, () => {
-//  console.log('Server listening on port 4000');
-//});
