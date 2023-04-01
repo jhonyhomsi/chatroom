566c6c3891bbfc3b63f3d44a0d03e1755f99d4cb
Jhony10-bit <johnny.aj.antonio@gmail.com>
2023-04-02 02:48:49 +0300

    FullUpdate

--- chat-server.js
+++ chat-server.js
@@ -7,81 +7,65 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const port = process.env.PORT || 4000;
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'chatroom';
+const collectionName = 'users';
+let db;
 
-async function connectToDatabase() {
-  const uri = 'mongodb+srv://jhony-33:Serafim12@cluster0.j3va4xj.mongodb.net/ChatsDatabase?retryWrites=true&w=majority'; // Replace with your own MongoDB Atlas URI
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  await client.connect();
-  return client.db().collection("ChatsLog");
-}
+// Connect to MongoDB
+MongoClient.connect(mongoUrl, (err, client) => {
+  if (err) {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
+  }
 
-let userCount = 0;
-
-wss.on('connection', async (ws) => {
-  console.log('a user connected');
-  userCount++;
-
-  const messagesCollection = await connectToDatabase();
-
-  ws.on('message', async (data) => {
-    console.log(`received message: ${data}`);
-
-    const messageObj = JSON.parse(data);
-
-    if (messageObj.type === 'join') {
-      const joinMessage = `${messageObj.name} has joined the chat`;
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: 'notification', message: joinMessage }));
-        }
-      });
-      userCount++;
-    } else if (messageObj.type === 'message') {
-      const message = `${messageObj.name}: ${messageObj.message}`;
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: 'message', message }));
-        }
-      });
-      await saveMessage(messagesCollection, messageObj);
-    }
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ type: 'userCount', count: userCount }));
-      }
-    });
-  });
-
-  ws.on('close', () => {
-    console.log('a user disconnected');
-    userCount--;
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ type: 'userCount', count: userCount }));
-      }
-    });
-  });
-
-  ws.send(JSON.stringify({ type: 'userCount', count: userCount }));
+  console.log('Connected to MongoDB successfully');
+  db = client.db(dbName);
 });
 
-async function saveMessage(collection, messageObj) {
-  const chatMessage = {
-    name: messageObj.name,
-    message: messageObj.message,
-    timestamp: new Date(),
-  };
-  collection.insertOne(chatMessage, (error, result) => {
-    if (error) {
-      console.log('Error saving message:', error); // add this line
-    } else {
-      console.log('Message saved successfully');
+// Broadcast the number of online users to all connected clients
+function broadcastUserCount() {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      db.collection(collectionName).countDocuments({ online: true }, (err, count) => {
+        if (err) {
+          console.error('Failed to retrieve online user count from database:', err);
+          return;
+        }
+        client.send(JSON.stringify({ type: 'userCount', count }));
+      });
     }
   });
 }
 
+// Handle new WebSocket connections
+wss.on('connection', (socket) => {
+  console.log('Client connected');
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  // Broadcast the number of online users to all connected clients on each new connection
+  broadcastUserCount();
+
+  // Handle incoming WebSocket messages
+  socket.on('message', (data) => {
+    console.log(`Received message from client: ${data}`);
+
+    // Broadcast the received message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+
+  // Handle WebSocket disconnections
+  socket.on('close', () => {
+    console.log('Client disconnected');
+
+    // Broadcast the number of online users to all connected clients on each disconnection
+    broadcastUserCount();
+  });
+});
+
+server.listen(8080, () => {
+  console.log('Server started on port 8080');
 });
