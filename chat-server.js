ee10f74efafea5b4dec76efa89c857fa2ace254f
jhony <johnny.aj.antonio@gmail.com>
2023-04-02 03:39:30 +0300

    Reversed

--- chat-server.js
+++ chat-server.js
@@ -63,8 +63,6 @@ wss.on('connection', async (ws) => {
     });
   });
 
-  const users = await getUsers(messagesCollection);
-  ws.send(JSON.stringify({ type: 'users', users }));
   ws.send(JSON.stringify({ type: 'userCount', count: userCount }));
 });
 
@@ -83,10 +81,6 @@ async function saveMessage(collection, messageObj) {
   });
 }
 
-async function getUsers(collection) {
-  const users = await collection.distinct("name");
-  return users;
-}
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
